8ff7f1141040cb64be96bac5716aebc5c252e298
tomoya <cj.0s0ttw.nsm4@gmail.com>
2024-06-22 19:37:37 +0900

    first commit

---
+++ SwiftUI_TCA/SwiftUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCAApp.swift
+//  SwiftUI_TCA
+//
+//  Created by 根岸智也 on 2024/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
